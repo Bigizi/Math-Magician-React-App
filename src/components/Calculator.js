7173406f34eb89a111a76f40efc49e20a2068407
Bigizi <bigizinduwayo@gmail.com>
2023-02-28 18:04:03 +0200

    Fix eslint and stylelint errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,18 +4,17 @@ import Button from './Button';
 const Calculator = () => {
   const styles = {
     h1: {
-        fontSize: "28px",
-        fontWeight: "bold"
+      fontSize: '28px',
+      fontWeight: 'bold',
     },
-};
+  };
   return (
     <div>
-    <h1 className='h1' style={styles.h1}>Math Magicians</h1>
-    <h2>Let's do some math!</h2>
-    <Button />
-  </div>
-  )
-
-  };
+      <h1 className="h1" style={styles.h1}>Math Magicians</h1>
+      <h2>Let us do some math!</h2>
+      <Button />
+    </div>
+  );
+};
 
 export default Calculator;
